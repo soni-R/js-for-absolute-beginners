a049133ab2ada71ea18991acad0f1f0b15d2401f
soni <soni.rukmaji@gmail.com>
2017-08-20 20:26:16 +0530

    Declaring variables

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-// console.log is used for debugging purposes
+//Global variables
 
-var name ='sam';
-console.log(name);
-
-var p = confirm('Are you sure');
-console.log(p);
-
-var number1 = 5,
-number2 = 10;
-result = number1 + number2;
-
-console.log(result);
+var name ='Benny',
+age = 30,
+money = 6.67,
+availableToday = false,
+haircolor,  //hairColor is undefined bcoz no value is defined to it.
+gender = null,
+nameAgain=name; //we can assign a variable name as value to another variable;
 
 
-/* experiment in console
- sam
- app.js:5 true
- 
- number1 - number2;
- -5
 
- number1 + ' ' +number2
-"5 10"
 
- name = "john"  //assigning a new value to a variable in console, which does not effect the main program, stays as is after refresh
- "john"
-
- name
- "john"
-
- name +' is '+ number2;
- "john is 10"  */
